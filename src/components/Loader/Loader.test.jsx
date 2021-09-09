6694b695f8c30010c12cc92ba9c97ec6a60cfca5
Maxim <mimaxm@yandex.ru>
2021-09-09 18:37:59 +0300

    snapshots update

--- src/components/Loader/Loader.test.jsx
+++ src/components/Loader/Loader.test.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { DataLoader } from './Loader';
 import { shallow } from 'enzyme';
+import toJson from "enzyme-to-json";
 
 describe('DataLoader testing', () => {
    const component = shallow(<DataLoader />);
-   test('DataLoader shoud have Loader', () => {
+   test('shoud have Loader', () => {
       expect(component.find('Loader')).toHaveLength(1)
    });
-   test('DataLoader shoud render without errors', () => {
-      expect(component).toMatchSnapshot();
+   test('shoud render without errors', () => {
+      expect(toJson(component)).toMatchSnapshot();
    })
 })
--- src/pages/About/About.test.jsx
+++ src/pages/About/About.test.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { About } from './About';
 import { shallow } from 'enzyme';
+import toJson from "enzyme-to-json";
 
 describe('About page testing', () => {
    const component = shallow(<About />);
    test('component should render', () => {
-      expect(component).toMatchSnapshot();
+      expect(toJson(component)).toMatchSnapshot();
    });
    test('component should include 3 "p"', () => {
-      expect(component.find('p')).toHaveLength(3);
+      expect(toJson(component.find('p'))).toHaveLength(3);
    });
 })
